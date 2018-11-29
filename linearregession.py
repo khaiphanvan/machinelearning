7498c2ccc5149c818f67b21c1c21819741df7cf0
khaiphanvan <khaipv.11@gmail.com>
2018-11-29 14:01:16 +0700

    linear regession

--- linearregession.py
+++ linearregession.py
@@ -1,2 +1,8 @@
 import numpy as np
 import pandas as panda
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import sklearn
+
+from sklearn.datasets import load_boston
+boston = load_boston()
